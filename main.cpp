56fb3ed275c3c5b59d3848e05ba6006ac2477205
Jeremy D Rhodes <jdrhodes@ecc-jaguar.csuchico.edu>
2018-10-22 16:45:56 -0700

    Counts how many pixels high and wide an image is

--- main.cpp
+++ main.cpp
@@ -8,6 +8,37 @@ using namespace std;
 //argv - each argument word, stored in an array
 int main(int argc, char* argv[])
 {
-  
+        string filename;
+        Bitmap image;
+        int height;
+        int width;
+
+        if(argc!=2)
+        {
+                cout<<"Please specify one image file!\n";
+        }
+        else
+        {
+                
+                filename = argv[1]; 
+        }
+        image.open(filename);
+
+        if(image.isImage() != true)
+        {
+                "Image file must be a bitmap with a 24-bit color depth.\n";
+        }
+        else
+        {
+                
+                vector <vector<Pixel>> imagePixels = image.toPixelMatrix();
+                
+                height = imagePixels.size();
+                width = imagePixels[0].size();
+
+                cout<<filename<< " is " << height << " pixels high and " << width << " pixels wide\n";
+        }
+        
+        
   return 0;
 }
